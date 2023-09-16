6ec95ba1429ddee851044907f38570c9f8c8da6b
AlexZ1207 <alex.zhenho@mail.mcgill.ca>
2023-09-15 23:54:03 -0400

    Add playerController.cs

--- Assets/Scripts/PlayerContorller.cs
+++ Assets/Scripts/PlayerContorller.cs
@@ -5,44 +5,76 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 1f;
-    public float gridSize = 0.5f;
+    public float gridSize = 1f; // Set this to your grid size
     private Vector3 targetPosition;
-
     private bool isMoving;
+    private Vector3 currentTransform;
+
+    public Animator animator;
+
 
     void Start()
     {
         targetPosition = transform.position;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (isMoving)
             return;
 
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
+        
+    
 
         // Prevent diagonal movement
-        if (h != 0) v = 0;
+        if (h != 0 && v != 0)
+        {
+            h = 0;
+            v = 0;
+        }
 
         if (h != 0 || v != 0)
         {
-            StartCoroutine(Move(new Vector3(h, v, 0)));
+            if(v == 0){
+                if(h>0) {
+                    transform.localScale = new Vector3(1,1,1);
+                    currentTransform = transform.localScale;
+                }
+                else {
+                    transform.localScale = new Vector3(-1,1,1);
+                    currentTransform = transform.localScale;
+                }
+            }else{
+                transform.localScale = currentTransform;
+            }
+            Move(new Vector3(h, v, 0));
+        }
+
+
+    }
+
+    void Move(Vector3 direction)
+    {
+        if (!isMoving)
+        {
+            targetPosition = transform.position + direction * gridSize;
+            isMoving = true;
+            animator.SetBool("isMoving",true);
         }
     }
 
-    System.Collections.IEnumerator Move(Vector3 direction)
+    void Update()
     {
-        isMoving = true;
-        targetPosition = transform.position + direction * gridSize;
-
-        while (transform.position != targetPosition)
+        if (transform.position != targetPosition)
         {
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
-            yield return null;
         }
-
-        isMoving = false;
+        else
+        {
+            isMoving = false;
+            animator.SetBool("isMoving",false);
+        }
     }
 }
